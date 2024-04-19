819b1e2b6f5908c9c5f86d7a217658bf9c3752ad
Korbinian Eckstein <korbinian90@gmail.com>
2024-04-19 15:57:11 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import logo from '/pwa-512x512.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // get maximum random number from user
+
+  const [maxRandomNumber, setMaxRandomNumber] = useState(6)
+  const [randomNumber, setRandomNumber] = useState(0)
+  const generateRandomNumber = () => {
+    setRandomNumber(1 + Math.floor(Math.random() * maxRandomNumber))
+  }
+
+  const [timeInterval, setTimeInterval] = useState(2000)
+  const [timer, setTimer] = useState(false)
+  const [progress, setProgress] = useState(0)
+
+  const progressSteps = 20
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (timer) {
+        setProgress(progress + 1)
+        if (progress >= progressSteps) {
+          generateRandomNumber()
+          setProgress(0)
+        }
+      }
+    }, timeInterval / progressSteps)
+    return () => clearInterval(intervalId)
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen flex items-center justify-center">
+        <div className="w-80">
+          <div className="text-center text-2xl font-bold p-8">Timed Random Number Generator</div>
+          <img src={logo} className="content-center" alt="Time logo" />
+          <div className="text-center text-2xl p-6 text-gray-300">Random Number </div>
+          <div className="text-center text-6xl border-4 p-6 rounded">{randomNumber}</div>
+          <label className="block p-1">
+            <span> Number of Outcomes </span>
+            <input className="border-2 border-gray-600 rounded-md bg-gray-700 h-6 w-20"
+              type="number"
+              value={maxRandomNumber}
+              onChange={(e) => setMaxRandomNumber(parseInt(e.target.value))}
+            />
+          </label>
+          <label className='block p-1'>
+            <span> Time Interval (ms) </span>
+            <input className="border-2 border-gray-600 rounded-md bg-gray-700 h-6 w-20"
+              type="number"
+              value={timeInterval}
+              step="100"
+              onChange={(e) => setTimeInterval(parseInt(e.target.value))}
+            />
+          </label>
+          <button className='w-full border-gray-400 rounded border-2 p-2 bg-gray-700 hover:bg-gray-800' onClick={generateRandomNumber}>Generate Random Number</button>
+          <button className='w-full border-gray-400 rounded border-2 p-2 bg-gray-700 hover:bg-gray-800' onClick={() => setTimer(!timer)}>
+            {timer ? 'Stop Timer' : 'Start Timer'}
+          </button>
+          <div className="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden">
+            <div className="flex flex-col justify-center rounded-full overflow-hidden bg-blue-600 text-xs text-white text-center whitespace-nowrap transition duration-500" style={{flex: progress / progressSteps}}></div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className='bg-gray-500' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'WebApp_TimedRandomNumber',
+  base: '/WebApp_TimedRandomNumber/',
   plugins: [react(),
   VitePWA({
     includeAssets: ['public/favicon.ico'],
