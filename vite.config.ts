9faf3dde445feedefba3756fff1cfc333e35bb4b
Korbinian Eckstein <korbinian90@gmail.com>
2024-04-19 12:38:13 +0200

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,23 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'WebApp_TimedRandomNumber',
   plugins: [react(),
-    VitePWA({
-      includeAssets: ['favicon.ico'],
-      manifest: {
-        name: 'Vite App',
-        short_name: 'ViteApp',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    })
-  ],
+  VitePWA({
+    includeAssets: ['favicon.ico'],
+    manifest: {
+      name: 'Timed Random Number',
+      short_name: 'TRN',
+      description: 'A simple app that generates a random number every n seconds',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  })
+  ]
 })
