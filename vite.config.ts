a89ba37c93094486740541313108a9de5fe3c538
Korbinian Eckstein <korbinian90@gmail.com>
2024-04-19 12:25:51 +0200

    make pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Vite App',
+        short_name: 'ViteApp',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
 })
